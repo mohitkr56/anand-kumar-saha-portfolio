691adfda00e6ede958f1d383ad6e3d488cb9978d
ANAND KUMAR SAHA <113061571+mohitkr56@users.noreply.github.com>
2026-06-06 17:21:48 +0530

    Added custom cursor effect

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Teaching from "./components/Teaching";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Cursor from "./components/Cursor";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <Cursor />
     </>
   );
 }
